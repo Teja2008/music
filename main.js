0998ed51ac6b27178cf1d857e74b4304f91a1a39
Teja2008 <86116952+Teja2008@users.noreply.github.com>
2022-11-26 20:06:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftwristy = 0;
 rightwristx= 0;
 rightwristy = 0;
 score_leftwrist= 0;
+score_rightwrist= 0;
 function setup(){
     canvas= createCanvas(600,500)
     canvas.center()
@@ -17,6 +18,7 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results)
         score_leftwrist= results[0].pose.keypoints[9].score
+        score_rightwrist= results[0].pose.keypoints[10].score
         console.log(score_leftwrist)
         leftwristx= results[0].pose.leftWrist.x
         leftwristy= results[0].pose.leftWrist.y
@@ -37,7 +39,7 @@ function draw(){
     image(video,0,0,600,500)
     fill("#F90000")
     stroke("#F90000")
-    circle(leftwristx,leftwristy,15)
+    
     InNumberleftwristy= Number(leftwristy)
     remove_decimals= floor(InNumberleftwristy)
     volume= remove_decimals/500
@@ -51,6 +53,33 @@ function draw(){
     document.getElementById("volume").innerHTML= volume
     song.setVolume(volume)
     }
+    
+
+    if(score_rightwrist >0.2){
+        circle(rightwristx,rightwristy,15)
+if(rightwristy>0 && rightwristy<= 100){
+    document.getElementById("speed").innerHTML= "speed= 0.5x"
+    song.rate(0.5)
+}
+else if(rightwristy>100 && rightwristy<= 200){
+    document.getElementById("speed").innerHTML= "speed= 1x"
+    song.rate(1)
+}
+else if(rightwristy>200 && rightwristy<= 300){
+    document.getElementById("speed").innerHTML= "speed= 1.5x"
+    song.rate(1.5)
+}
+else if(rightwristy>300 && rightwristy<= 400){
+    document.getElementById("speed").innerHTML= "speed= 2x"
+    song.rate(2)
+}
+else if(rightwristy>400 ){
+    document.getElementById("speed").innerHTML= "speed= 2.5x"
+    song.rate(2.5)
+}
+    }
+    
+
 }
 function play(){
     song.play();
